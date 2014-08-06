2e279a7620b84e169b545a8a9c575027ebda9ea3
seanlancaster <n43s@hotmail.com>
2014-08-06 14:45:44 +1200

    Simple login page and link between login and landing pages

--- Ticket Droid/gen/com/trout/ticketdroid/R.java
+++ Ticket Droid/gen/com/trout/ticketdroid/R.java
@@ -25,29 +25,38 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080007;
+        public static final int emailaddress=0x7f080004;
+        public static final int forgotButton=0x7f080003;
         public static final int introText=0x7f080001;
         public static final int loginButton=0x7f080002;
+        public static final int password=0x7f080005;
         public static final int registerButton=0x7f080000;
-        public static final int textView1=0x7f080003;
+        public static final int textView1=0x7f080006;
     }
     public static final class layout {
         public static final int activity_landing_page=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_login_page=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
         public static final int landing_page=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int login_page=0x7f070001;
+        public static final int main=0x7f070002;
     }
     public static final class string {
         public static final int TEST=0x7f050002;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int emailText=0x7f05000a;
+        public static final int forgotButton=0x7f050009;
         public static final int hello_world=0x7f050004;
         public static final int introText=0x7f050005;
         public static final int loginButton=0x7f050006;
+        public static final int passwordText=0x7f05000b;
         public static final int registerButton=0x7f050007;
         public static final int title_activity_landing_page=0x7f050003;
+        public static final int title_activity_login_page=0x7f050008;
     }
     public static final class style {
         /** 
