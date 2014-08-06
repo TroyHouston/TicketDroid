49d8ad1f058be1781edd0541284f54074af89e11
seanlancaster <n43s@hotmail.com>
2014-08-06 15:54:18 +1200

    Register page and start of ticket list page

--- Ticket Droid/gen/com/trout/ticketdroid/R.java
+++ Ticket Droid/gen/com/trout/ticketdroid/R.java
@@ -25,7 +25,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f080008;
+        public static final int confirmPassword=0x7f080007;
         public static final int emailaddress=0x7f080004;
         public static final int forgotButton=0x7f080003;
         public static final int introText=0x7f080001;
@@ -38,16 +39,21 @@ public final class R {
         public static final int activity_landing_page=0x7f030000;
         public static final int activity_login_page=0x7f030001;
         public static final int activity_main=0x7f030002;
+        public static final int activity_register_page=0x7f030003;
+        public static final int activity_ticket_list_page=0x7f030004;
     }
     public static final class menu {
         public static final int landing_page=0x7f070000;
         public static final int login_page=0x7f070001;
         public static final int main=0x7f070002;
+        public static final int register_page=0x7f070003;
+        public static final int ticket_list_page=0x7f070004;
     }
     public static final class string {
         public static final int TEST=0x7f050002;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int confirmPasswordText=0x7f05000d;
         public static final int emailText=0x7f05000a;
         public static final int forgotButton=0x7f050009;
         public static final int hello_world=0x7f050004;
@@ -57,6 +63,8 @@ public final class R {
         public static final int registerButton=0x7f050007;
         public static final int title_activity_landing_page=0x7f050003;
         public static final int title_activity_login_page=0x7f050008;
+        public static final int title_activity_register_page=0x7f05000c;
+        public static final int title_activity_ticket_list_page=0x7f05000e;
     }
     public static final class style {
         /** 
