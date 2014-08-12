9fc5a80b900905fe40f9c9c8fedf0c753a16a229
seanlancaster <n43s@hotmail.com>
2014-08-13 10:02:26 +1200

    Linked navigate from login to ticket screen

--- Ticket Droid/src/com/trout/ticketdroid/LoginPage.java
+++ Ticket Droid/src/com/trout/ticketdroid/LoginPage.java
@@ -1,9 +1,11 @@
 package com.trout.ticketdroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class LoginPage extends Activity {
 
@@ -30,5 +32,11 @@ public class LoginPage extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public void goToTicketPage(View view){
+		Intent intent = new Intent(this, TicketListPage.class);
+
+		startActivity(intent);
 	}
 }
